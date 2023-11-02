accea38f125cda4405ca068436a42353f7a17db8
Solomon Amsalu <solamsalu@gmail.com>
2023-11-02 15:53:13 +0300

    Task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
-""" Task 0: Regex-ing """
+""" Use of regex in replacing occurrences of certain field values """
 
+from typing import List
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
-    """ a function  that returns the log message obfuscated """
-    pattern = f'({"|".join(fields)}){separator}[^{separator}]*'
-    replacement = f'\\1{separator}{redaction}'
-    return re.sub(pattern, replacement, message)
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ Returns a log message obfuscated """
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
