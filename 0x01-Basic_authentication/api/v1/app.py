e5018595e77a15a110a4369f642e68a1642eb507
Solomon Amsalu <solamsalu@gmail.com>
2023-11-07 22:31:53 +0300

    Task 2

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -35,6 +35,30 @@ def forbidden(error) -> str:
     return jsonify({"error": "Forbidden"}), 403
 
 
+@app.before_request
+def before_request() -> str:
+    """Execute before each request
+
+        Return:
+            String or nothing
+    """
+    if auth is None:
+        return
+
+    expath = ['/api/v1/status/',
+              '/api/v1/unauthorized/',
+              '/api/v1/forbidden/']
+
+    if not (auth.require_auth(request.path, expath)):
+        return
+
+    if (auth.authorization_header(request)) is None:
+        abort(401)
+
+    if (auth.current_user(request)) is None:
+        abort(403)
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
