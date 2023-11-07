033a32e40132733e1a8c2c58d1ffa5b6c720cdcb
Solomon Amsalu <solamsalu@gmail.com>
2023-11-08 00:53:33 +0300

    Task 13

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -17,13 +17,28 @@ class Auth:
         """
         returns False - path and excluded_paths
         """
-        check = path
-        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+        # If path is None, return True
+        if not path:
             return True
-        if path[-1] != "/":
-            check += "/"
-        if check in excluded_paths or path in excluded_paths:
-            return False
+        # If excluded_paths is None or empty, return True
+        if not excluded_paths:
+            return True
+        # Remove the trailing slash from the path
+        path = path.rstrip("/")
+        # Check if path is in excluded_paths and return False if path is
+        # in excluded_paths
+        # Loop through excluded paths
+        for excluded_path in excluded_paths:
+            # Check if given path starts with excluded path, with * at the end
+            if excluded_path.endswith("*") and \
+                    path.startswith(excluded_path[:-1]):
+                # Return False if path starts with excluded path with * at end
+                return False
+            # Check if the given path is equal to the excluded path
+            elif path == excluded_path.rstrip("/"):
+                # Return False if the path is equal to the excluded path
+                return False
+        # If path is not in excluded_paths, return True
         return True
 
     def authorization_header(self, request=None) -> str:
