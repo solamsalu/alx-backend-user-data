3747d38cc24be48dffe1e9f56b425a9b43c7a74a
Solomon Amsalu <solamsalu@gmail.com>
2023-11-07 22:14:22 +0300

    Task 2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -36,7 +36,7 @@ def unauthorized_error() -> str:
 
 
 @app_views.route('/forbidden/', strict_slashes=False)
-def forbidden() -> None:
+def forbidden_error() -> str:
     """GET /api/v1/forbidden
     Return:
       - Forbidden error.
