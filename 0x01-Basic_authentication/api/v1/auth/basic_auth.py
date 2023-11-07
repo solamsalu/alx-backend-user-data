f685f672c1064c653c9b3012871857167abddbf5
Solomon Amsalu <solamsalu@gmail.com>
2023-11-08 00:37:09 +0300

    Task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -65,7 +65,8 @@ class BasicAuth(Auth):
         user_creds = self.extract_user_credentials(decoded)
         return self.user_object_from_credentials(*user_creds)
 
-    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> User:
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> User:
         """Return the User instance based on email and password."""
         if user_email is None or not isinstance(user_email, str):
             return None
@@ -73,12 +74,12 @@ class BasicAuth(Auth):
             return None
 
         users = User.search({'email': user_email})
-        
+
         if not users:
             return None
-        
+
         # Verify user password
-        user = users[0]  # Assuming the search returns a list and we take the first match
+        user = users[0]
         if user.is_valid_password(user_pwd):
             return user
         else:
