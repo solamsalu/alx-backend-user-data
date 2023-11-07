2b14b00fc593833ce12e5759dac4dd2c1f321f76
Solomon Amsalu <solamsalu@gmail.com>
2023-11-08 00:25:56 +0300

    Task 6

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,18 +2,22 @@
 """
 Route module for the API
 """
+import os
 from os import getenv
-from api.v1.views import app_views
+from typing import Tuple
+
+from flask import Flask, abort, jsonify, request
+from flask_cors import CORS, cross_origin
+
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
+from api.v1.views import app_views
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
 
 # Import BasicAuth if AUTH_TYPE is set to 'basic_auth'
 auth_type = getenv("AUTH_TYPE", default="none")
