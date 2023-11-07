c8771e66bf25cb989e62e22b0f2055e42f720b3a
Solomon Amsalu <solamsalu@gmail.com>
2023-11-07 23:41:29 +0300

    Task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,37 +7,22 @@ from typing import List, TypeVar
 
 
 User = TypeVar('User')
-
-
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Determine if the path requires authentication."""
-        """Determine if the path requires authentication."""
-        # Returns True if path is None
-        if path is None:
+        if not excluded_paths or not path:
             return True
-
-        # Returns True if excluded_paths is None or empty
-        if not excluded_paths:
-            return True
-
-        # Ensure the path has a trailing slash for comparison
-        if not path.endswith('/'):
-            path += '/'
-
-        for pattern in excluded_paths:
-            # Ensure the pattern has a trailing slash for comparison
-            if not pattern.endswith('/'):
-                pattern += '/'
-            if path == pattern:
+        path = path if path.endswith('/') else path + '/'
+        for excluded_path in excluded_paths:
+            if path.startswith(excluded_path.strip("*")):
                 return False
-
-        # If the path is not in excluded_paths, return True
         return True
 
     def authorization_header(self, request=None) -> str:
         """Get the authorization header from the request."""
-        return None  # This method will be implemented later
+        if not request or 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> User:
         """Get the current user from the request."""
