c1e28d90b62353e52c8303afbc097bc3316b1459
Solomon Amsalu <solamsalu@gmail.com>
2023-11-07 23:17:03 +0300

    Task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,16 +12,28 @@ User = TypeVar('User')
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Determine if the path requires authentication."""
-        if path is None or not excluded_paths:
+        # If path is None, return True
+        if not path:
             return True
-
-        # Normalize the path to ensure slash tolerance
-        path = path.strip('/')
-        for pattern in excluded_paths:
-            # Normalize the pattern to ensure slash tolerance
-            pattern = pattern.strip('/')
-            if path == pattern or path.startswith(pattern + '/'):
+        # If excluded_paths is None or empty, return True
+        if not excluded_paths:
+            return True
+        # Remove the trailing slash from the path
+        path = path.rstrip("/")
+        # Check if path is in excluded_paths and return False if path is
+        # in excluded_paths
+        # Loop through excluded paths
+        for excluded_path in excluded_paths:
+            # Check if given path starts with excluded path, with * at the end
+            if excluded_path.endswith("*") and \
+                    path.startswith(excluded_path[:-1]):
+                # Return False if path starts with excluded path with * at end
                 return False
+            # Check if the given path is equal to the excluded path
+            elif path == excluded_path.rstrip("/"):
+                # Return False if the path is equal to the excluded path
+                return False
+        # If path is not in excluded_paths, return True
         return True
 
     def authorization_header(self, request=None) -> str:
