0a07dcf4797c3599d21a6adba3a0524a71ad2255
Solomon Amsalu <solamsalu@gmail.com>
2023-11-16 16:37:35 +0300

    Advanced Task

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,144 +1,202 @@
 #!/usr/bin/env python3
-"""A simple Flask app with user authentication features.
+"""Module for simple end-to-end (E2E) integration tests for `app.py`.
 """
-import logging
 
-from flask import Flask, abort, jsonify, redirect, request
+import requests
 
-from auth import Auth
+from app import AUTH
 
-logging.disable(logging.WARNING)
-
-
-AUTH = Auth()
-app = Flask(__name__)
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+BASE_URL = "http://0.0.0.0:5000"
 
 
-@app.route("/", methods=["GET"], strict_slashes=False)
-def index() -> str:
-    """GET /
-    Return:
-        - JSON payload containing a welcome message.
+def register_user(email: str, password: str) -> None:
+    """Test the registration of a user.
+
+    Args:
+        email (str): The user's email.
+        password (str): The user's password.
     """
-    return jsonify({"message": "Bienvenue"})
+    url = "{}/users".format(BASE_URL)
+    data = {
+        "email": email,
+        "password": password
+    }
+    # Attempt to register a new user
+    response = requests.post(url, data=data)
+    # print("RESPONSE.STATUSCODE: {}".format(response.status_code))
+    # Assert that the response has a 200 status code
+    assert response.status_code == 200
+    # Assert that the response JSON payload contains the correct data
+    assert response.json() == {"email": email, "message": "user created"}
+    # Attempt to register the same user again
+    response = requests.post(url, data=data)
+    # Assert that the response has a 400 status code
+    assert response.status_code == 400
+    # Assert that the response JSON payload contains the correct data
+    assert response.json() == {"message": "email already registered"}
 
 
-@app.route("/users", methods=["POST"], strict_slashes=False)
-def users() -> str:
-    """POST /users
-    Return:
-        - JSON payload of the form containing various information.
+def log_in_wrong_password(email: str, password: str) -> None:
+    """Test logging in with wrong password.
+
+    Args:
+        email (str): The email address of the user to log in.
+        password (str): The user's password.
     """
-    # Get the email and password from form data
-    email, password = request.form.get("email"), request.form.get("password")
-    try:
-        # Register the user
-        AUTH.register_user(email, password)
-        # Respond with the following JSON payload:
-        # {"email": "<registered email>", "message": "user created"}
-        return jsonify({"email": email, "message": "user created"})
-    # If the user is already registered, catch the exception and return a
-    # JSON payload of the form: {"message": "email already registered"}
-    # and return a 400 status code
-    except ValueError:
-        return jsonify({"message": "email already registered"}), 400
+    # Send a POST request to the login endpoint with the provided email
+    # and an incorrect password
+    url = "{}/sessions".format(BASE_URL)
+    data = {
+        "email": email,
+        "password": password
+    }
+    response = requests.post(url, data=data)
+    # print("RESPONSE.STATUSCODE: {}".format(response.status_code))
+    # Ensure that the response status code is 401 (Unauthorized)
+    assert response.status_code == 401
 
 
-@app.route("/sessions", methods=["POST"], strict_slashes=False)
-def login() -> str:
-    """POST /sessions
-    Return:
-        - JSON payload of the form containing login info.
+def profile_unlogged() -> None:
+    """Tests behavior of trying to retrieve profile information
+    while being logged out.
     """
-    # Get user credentials from form data
-    email, password = request.form.get("email"), request.form.get("password")
-    # Check if the user's credentials are valid
-    if not AUTH.valid_login(email, password):
-        abort(401)
-    # Create a new session for the user
-    session_id = AUTH.create_session(email)
-    # Construct a response with a JSON payload
-    response = jsonify({"email": email, "message": "logged in"})
-    # Set a cookie with the session ID on the response
-    response.set_cookie("session_id", session_id)
-    # Return the response
-    return response
+    url = "{}/profile".format(BASE_URL)
+    # Make a GET request to the /profile endpoint
+    response = requests.get(url)
+    # Assert that the response status code is 403 Forbidden
+    assert response.status_code == 403
 
 
-@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
-def logout() -> str:
-    """DELETE /sessions
-    Return:
-        - A redirect if successful
+def profile_logged(session_id: str) -> None:
+    """Tests retrieving profile information whilst logged in.
+
+    Args:
+        session_id (str): The session ID of the logged in user.
     """
-    # Get the session ID from the "session_id" cookie in the request
-    session_id = request.cookies.get("session_id")
-    # Retrieve the user associated with the session ID
+    # Send a GET request to the /profile endpoint with the session ID cookie
+    url = "{}/profile".format(BASE_URL)
+    cookies = {
+        "session_id": session_id
+    }
+    response = requests.get(url, cookies=cookies)
+    # print("RESPONSE.STATUSCODE: {}".format(response.status_code))
+    # Assert that the response has a status code of 200
+    assert response.status_code == 200
+    # Parse the JSON payload from the response
+    payload = response.json()
+    # Assert that the email is present in the response payload
+    assert "email" in payload
+    # Assert that email in response payload matches logged in user's email
     user = AUTH.get_user_from_session_id(session_id)
-    # If no user is found, abort the request with a 403 Forbidden error
-    if user is None:
-        abort(403)
-    # Destroy the session associated with the user
-    AUTH.destroy_session(user.id)
-    # Redirect to the home route
-    return redirect("/")
+    assert user.email == payload["email"]
 
 
-@app.route("/profile", methods=["GET"], strict_slashes=False)
-def profile() -> str:
-    """GET /profile
-    Return:
-        - A JSON payload containing the email if successful.
+def log_out(session_id: str) -> None:
+    """Tests tests the process of logging out from a session.
+
+    Args:
+        session_id (str): The session ID of the user to log out.
     """
-    # Get the session ID from the "session_id" cookie in the request
-    session_id = request.cookies.get("session_id")
-    # Retrieve the user associated with the session ID
-    user = AUTH.get_user_from_session_id(session_id)
-    # If no user is found, abort the request with a 403 Forbidden error
-    if user is None:
-        abort(403)
-    # Return the user's email as a JSON payload
-    return jsonify({"email": user.email})
+    # Make a request to log out the user
+    url = "{}/sessions".format(BASE_URL)
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = {
+        "session_id": session_id
+    }
+    response = requests.delete(url, headers=headers, cookies=data)
+    # print("RESPONSE.STATUSCODE: {}".format(response.status_code))
+    # Assert that the response has the expected status code and payload
+    assert response.status_code == 200
 
 
-@app.route("/reset_password", methods=["POST"], strict_slashes=False)
-def get_reset_password_token() -> str:
-    """POST /reset_password
-    Return:
-        - A JSON payload containing the email & reset token if successful.
+def reset_password_token(email: str) -> str:
+    """Tests the process of requesting a password reset.
+
+    Args:
+        email (str): The email to request password reset for.
     """
-    # Retrieve the email from the form data
-    email = request.form.get("email")
-    try:
-        # Attempt to generate a reset token for the given email
-        reset_token = AUTH.get_reset_password_token(email)
-    except ValueError:
-        # If the email is not found in the database, raise a 403 error
-        abort(403)
-    # Return a JSON payload containing the email and reset token
-    return jsonify({"email": email, "reset_token": reset_token})
+    # Make a POST request to the "/reset_password" endpoint
+    url = "{}/reset_password".format(BASE_URL)
+    data = {
+        "email": email
+    }
+    response = requests.post(url, data=data)
+    # Assert that the response has a 200 status code
+    assert response.status_code == 200
+    # Assert that the response contains the email & reset token as JSON payload
+    assert "email" in response.json()
+    assert response.json()["email"] == email
+    # assert response.json() == {"email": email}
+    # Extract the reset token from the response
+    reset_token = response.json()["reset_token"]
+    # Return the reset_token
+    return reset_token
 
 
-@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
-def update_password() -> str:
-    """PUT /reset_password
-    Return:
-        - The user's updated password.
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    """Tests updating a user's password.
+
+    Args:
+        email (str): The email of the user whose password should be updated.
+        reset_token (str): The reset token generated for the user.
+        new_password (str): The new password to set for the user.
     """
-    # Retrieve the email, reset_token and new_password from the form data
-    email = request.form.get("email")
-    reset_token = request.form.get("reset_token")
-    new_password = request.form.get("new_password")
-    try:
-        # Attempt to update the password with the new password
-        AUTH.update_password(reset_token, new_password)
-    except ValueError:
-        # If the reset token is invalid, return an HTTP 403 error
-        abort(403)
-    # If the password was successfully updated, return a JSON object with the
-    # user's email and a success message.
-    return jsonify({"email": email, "message": "Password updated"})
+    url = "{}/reset_password".format(BASE_URL)
+    data = {
+        "email": email,
+        "reset_token": reset_token,
+        "new_password": new_password
+    }
+    response = requests.put(url, data=data)
+    # Assert the status code of the response is 200
+    assert response.status_code == 200
+    # Assert the message in the response matches the expected message
+    assert response.json()["message"] == "Password updated"
+    # Assert the email in the response matches the email passed in
+    assert response.json()["email"] == email
+
+
+def log_in(email: str, password: str) -> str:
+    """Tests logging in.
+
+    Args:
+        email (str): The email address of the user to log in.
+        password (str): The user's password.
+    """
+    url = "{}/sessions".format(BASE_URL)
+    data = {
+        "email": email,
+        "password": password
+    }
+    # Make a POST request to the login endpoint with the user's credentials
+    response = requests.post(url, data=data)
+    # Check if the response status code is 401 in case of invalid credentials
+    if response.status_code == 401:
+        return "Invalid credentials"
+    # Check if the response status code is 200 in case of valid credentials
+    assert response.status_code == 200
+    # Check if email & message keys are present in the response JSON payload
+    response_json = response.json()
+    assert "email" in response_json
+    assert "message" in response_json
+    # Check if email in response JSON payload matches the email in the request
+    assert response_json["email"] == email
+    # Return the session ID from the response cookie
+    return response.cookies.get("session_id")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
